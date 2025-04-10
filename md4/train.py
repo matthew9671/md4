2876896fdc2868633fc59b5c2c6d420da7c825b3
yixiuz <yixiuz@stanford.edu>
2025-04-10 23:09:19 +0000

    Last attempt

--- md4/train.py
+++ md4/train.py
@@ -97,7 +97,8 @@ def _get_checkpoint_manager(
         checkpoint_dir,
         checkpointers=checkpointers,
         options=orbax_checkpoint.CheckpointManagerOptions(
-            create=True, keep_period=keep_period,
+            create=False, 
+            keep_period=keep_period,
             enable_async_checkpointing=False,
             multiprocessing_options=orbax_checkpoint.checkpoint_manager.MultiprocessingOptions(primary_host=0, active_processes={0,})
             # async_options=orbax_checkpoint.AsyncOptions(barrier_sync_fn=_dummy_barrier_sync_fn)
