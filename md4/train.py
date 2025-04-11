958ee13c36c1a2a30b8cb7b453df6049ed5a1513
yixiuz <yixiuz@stanford.edu>
2025-04-11 01:29:08 +0000

    Switching to single host checkpoint

--- md4/train.py
+++ md4/train.py
@@ -683,7 +683,8 @@ def train_and_evaluate(config: ml_collections.ConfigDict, workdir: epath.PathLik
     checkpoint_dir = str(workdir / "checkpoints")
     # The vdm code initalizes two checkpoints, one for loading and one for saving
     # which I don't understand
-    ckpt = checkpoint.MultihostCheckpoint(checkpoint_dir)
+    # ckpt = checkpoint.MultihostCheckpoint(checkpoint_dir)
+    ckpt = checkpoint.Checkpoint(checkpoint_dir)
     checkpoint_to_restore = ckpt.get_latest_checkpoint_to_restore_from()
     
     if checkpoint_to_restore:
